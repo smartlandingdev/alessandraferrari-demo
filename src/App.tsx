c987be9d09fc0f45bf53f82f855b8cfc787dc176
Majuks <mariajnascimento04@gmail.com>
2025-12-01 02:25:02 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -413,43 +413,6 @@ function App() {
       {/* Contato */}
       <section id="contato" className="section-contato">
         <div className="container">
-          <div className="contato-items">
-            <div className="contato-item">
-              <div className="contato-icon">
-                <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                  <circle cx="12" cy="10" r="3"></circle>
-                </svg>
-              </div>
-              <div>
-                <h4>Endereço</h4>
-                <p>Rua São Paulo, 707, Sl. 101<br/>Edifício Mariga, Centro<br/>Cascavel/PR</p>
-              </div>
-            </div>
-            <div className="contato-item">
-              <div className="contato-icon">
-                <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                  <polyline points="22,6 12,13 2,6"></polyline>
-                </svg>
-              </div>
-              <div>
-                <h4>E-mail</h4>
-                <p>contato@advalessandraferrari.com.br<br/>advalessandraferrari@gmail.com</p>
-              </div>
-            </div>
-            <div className="contato-item">
-              <div className="contato-icon">
-                <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                </svg>
-              </div>
-              <div>
-                <h4>Telefone</h4>
-                <p>(45) 3306-8111 / (45) 3306-8112<br/>(45) 9 9900-6007 (WhatsApp)</p>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
 
